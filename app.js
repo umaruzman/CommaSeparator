56c3ab4c7f8d58487d825a85e604dbcafc25868c
Umar Uzman <umaruzman.lk@gmail.com>
2020-07-22 02:36:52 +0530

    Developed Code Added

---
+++ app.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    minHeight: 600,
+    minWidth: 900,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+
+
+  //Menu Section
+
+  Menu.setApplicationMenu(null);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ src/index.js
@@ -0,0 +1,167 @@
+// Function Start Runs after the window completes loading...
+
+function start () {
+
+    /*
+        [START] Dropdown Control
+    */
+
+    var btnSeparatorType = document.getElementById('btnSeparatorType');
+
+    // Dropdown Comma(,) Selection
+    var ddComma = document.getElementById('ddComma');
+
+    ddComma.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = ",";
+    });
+
+    // Dropdoen Colon(:) Selection
+    var ddColon = document.getElementById('ddColon');
+
+    ddColon.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = ":";
+    });
+
+    // Dropdoen SemiColon(;) Selection
+    var ddSemiColon = document.getElementById('ddSemiColon');
+
+    ddSemiColon.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = ";";
+    });
+
+    // Dropdoen Dash(-) Selection
+    var ddDash = document.getElementById('ddDash');
+
+    ddDash.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = "-";
+    });
+
+    // Dropdoen Slash(/) Selection
+    var ddSlash = document.getElementById('ddSlash');
+
+    ddSlash.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = "/";
+    });
+
+    // Dropdoen HLine(|) Selection
+    var ddHLine = document.getElementById('ddHLine');
+
+    ddHLine.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = "|";
+    });
+
+    // Dropdoen Space( ) Selection
+    var ddSpace = document.getElementById('ddSpace');
+
+    ddSpace.addEventListener('click', function () {
+        btnSeparatorType.innerHTML = "Space";
+    });
+
+    
+    /*
+        [END] Dropdown Control
+    */
+
+    /*
+        [START] Separate Multiline Text Button (>>)
+    */
+
+    var separatorType = document.getElementById('btnSeparatorType').innerHTML;
+        
+    var btnSeperateMultiline = document.getElementById('btnSeperateMultiline');
+
+    btnSeperateMultiline.addEventListener('click', function () {
+        separatorType = document.getElementById('btnSeparatorType').innerHTML;
+        if (separatorType == "Space") {
+            document.getElementById('txtOutput').value = separateMultiline(" ");
+        } else {
+            document.getElementById('txtOutput').value = separateMultiline(separatorType);
+        }
+
+    });
+
+    /*
+        [END] Separate Multiline Text Button (>>)
+    */
+
+    /*
+        [START] Separate Singleline Text Button (<<)
+    */
+        
+   var btnSeperateSingleLine = document.getElementById('btnSeperateSingleLine');
+
+   btnSeperateSingleLine.addEventListener('click', function () {
+        separatorType = document.getElementById('btnSeparatorType').innerHTML;
+        if (separatorType == "Space") {
+                document.getElementById('txtInput').value = separateSingleLine(" ");
+        } else {
+                document.getElementById('txtInput').value = separateSingleLine(separatorType);
+        }
+   });
+
+   /*
+       [END] Separate Singleline Text Button (<<)
+   */
+
+   /*
+       [START] Clear Button
+   */
+
+    var btnClear = document.getElementById('btnClear');
+
+    btnClear.addEventListener('click', function () {
+        document.getElementById('txtInput').value = "";
+        document.getElementById('txtOutput').value = "";
+    });
+
+   /*
+       [END] Clear Button
+   */
+
+}
+
+
+// Function for Separate Multiline Text
+
+
+function separateMultiline (separator) {
+    var inputText = document.getElementById('txtInput').value;
+
+    var inputTextArr = inputText.split("\n");
+
+    var outputText = "";
+
+    for (i of inputTextArr) {
+        outputText = outputText + i.toLocaleString() + separator.toLocaleString();
+    }
+
+    return outputText.slice(0, outputText.length - 1);
+}
+
+
+// Function for Separate SingleLine Text
+
+
+function separateSingleLine (separator) {
+    var outputText = document.getElementById('txtOutput').value;
+
+    var textArr = outputText.split(separator);
+
+    var resultText = "";
+
+    for (i of textArr) {
+        resultText = resultText + i.toLocaleString() + "\n";
+    }
+
+    return resultText.slice(0, resultText.length - 1);
+}
+
+
+// This calls the Start Function after loading the window..
+
+var body = document.getElementsByTagName('body')[0];
+
+body.onload = function () {
+    start();
+};
+
